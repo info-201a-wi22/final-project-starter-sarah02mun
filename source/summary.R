e48c624cd8ef4a1084ee588aced97d89f9410abf
Vianne Buinguyen <vianneb@uw.edu>
2022-02-15 15:44:54 -0800

    added summary file to source and updated csv dataset

---
+++ source/summary.R
@@ -0,0 +1,33 @@
+#load packages
+library("dplyr")
+
+#set wd
+setwd("C:/Users/viann/OneDrive/Desktop/INFO-201/final-project-starter-sarah02mun/data")
+
+#load dataset
+min_wage_data <- read.csv("Minimum-Wage-Data.csv",
+                            stringsAsFactors = FALSE)
+#summary list
+summary_info <- list()
+
+#Washington state minimum wage over time
+summary_info$wa_min_wage <- min_wage_data %>%
+  filter(State=="Washington") %>% 
+  select(State.Minimum.Wage)
+
+#Federal minimum wage over time
+summary_info$fed_min_wage <- min_wage_data %>% 
+  group_by(Year) %>% 
+  summarize(fed_min_wage = mean(Federal.Minimum.Wage)) %>%
+  select(fed_min_wage)
+  
+#Consumer price index over time
+summary_info$avg_cpi <- min_wage_data %>% group_by(Year) %>% 
+  summarize(CPI = mean(CPI.Average)) %>% 
+  select(CPI)
+
+#Ratio of Washington state minimum wage to CPI over time
+summary_info$wa_min_wage_to_cpi <- summary_info$wa_min_wage/summary_info$avg_cpi
+
+#Ratio of Federal minimum wage to CPI over time
+summary_info$fed_min_wage_to_cpi <- summary_info$fed_min_wage/summary_info$avg_cpi
