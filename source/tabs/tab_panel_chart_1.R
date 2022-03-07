b391b5f9d2ba3a878b22e7f4cb02188fbb2c3c6d
Vianne Buinguyen <vianneb@uw.edu>
2022-03-06 19:25:48 -0800

    changed chart 1 to convey minimum wage/cpi instead of minimum wage

--- source/tabs/tab_panel_chart_1.R
+++ source/tabs/tab_panel_chart_1.R
@@ -6,7 +6,8 @@ library("plotly")
 build_map <- function(data, year) {
   #filter data to get minimum wage of all states in year
   df <- data %>%
-    filter(Year==!!year)
+    filter(Year==!!year) %>%
+    mutate(Min.Wage.To.CPI = round(State.Minimum.Wage/CPI.Average, 3))
   
   #keep only US states
   df <- df[-c(9, 12, 42, 47), ]
@@ -28,12 +29,12 @@ build_map <- function(data, year) {
   #create map plot centered on the US
   fig <- plot_geo(df, locationmode = 'USA-states')
   fig <- fig %>% add_trace(
-    z = ~State.Minimum.Wage, locations = ~code,
-    color = ~State.Minimum.Wage, colors = "Purples"
+    z = ~Min.Wage.To.CPI, locations = ~code,
+    color = ~Min.Wage.To.CPI, colors = "Purples"
   )
-  fig <- fig %>% colorbar(title = "Dollars USD")
+  fig <- fig %>% colorbar(title = "Minimum Wage/CPI")
   fig <- fig %>% layout(
-    title = paste0("US State Minimum Wages in ", year),
+    title = paste0("Minimum Wage/CPI by US State in ", year),
     geo = g
   )
 }
