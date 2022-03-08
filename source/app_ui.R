93f473c37f2fc852e968acf80fd8843eb8065344
fatimaalsinai <fatimaalsinai@gmail.com>
2022-03-08 13:50:24 -0800

    added introduction

--- source/app_ui.R
+++ source/app_ui.R
@@ -1,6 +1,9 @@
 # load packages 
 library("shiny")
 
+#set working directory 
+setwd("~/Info-project/final-project-starter-sarah02mun")
+
 # load data
 min_wage_data <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-sarah02mun/main/data/Minimum-Wage-Data.csv", 
                           stringsAsFactors = FALSE)
@@ -80,7 +83,25 @@ intro_panel <- tabPanel(
   "Introduction",
   titlePanel("Introduction"),
   fluidPage(
-    tags$img(src = "../docs/Minimum-Wage.jpg")
+    tags$img(src = "../docs/Minimum-Wage.jpg"),
+    p("The minimum wage varies across the United States. It has re-entered the forefront of policy discourse as federal and state proposals range from as low as $7.25 to as high as $17.25. The federal minimum wage has remained stagnant for several
+years; however, the state-level minimum wage has changed frequently over time. High state-level minimum wages are related to local economic conditions or the standard of living in that state. The change in minimum wage generates variation 
+in the consumer price index and allows us to identify the relationship between the minimum wage changes and inflation, as well as the consumption growth."),
+    
+    
+    P("Our project studies the changes in minimum wage at the state and federal level. We conducted our research using a dataset titled, U.S. Minimum Wage by State from 1968 to 2020, from Kaggle.
+Through this dataset, we were able to examine how the changes in minimum wage affects the consumer index price (CPI). We mainly focused on data collected in 2020 related to the average CPI, federal minimum wage, and 
+and state minimum wage to answer our research questions."),
+    
+    
+    h1("Research Questions"),
+    
+    P("How has the US states' minimum wage changed over time?"),
+    p("How has the federal minimum wage changed compared to consumer price index over time?"),
+    p("How does the ratio of the US states' minimum wage to consumer price index change over time?"),
+    
+    p("Our data analysis answered our research questions and our findings are shown through different types of charts."),
+    
   )
 )
 
